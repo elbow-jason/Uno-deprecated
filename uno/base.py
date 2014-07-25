07e73328461c2abbf197a5efc794eba817867bf1
jlgoldb2 <jlgoldb2@asu.edu>
2014-07-24 18:26:52 -0700

    import error

--- uno/base.py
+++ uno/base.py
@@ -1,28 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import markup
-from flask import Markup
+from markupsafe import Markup
 
 try:
     from .errors import error
+    print "SUPER"
 except:
-    if __name__ == '__main__':
-        class ErrorObj(object):
-            pass
-
-        error = ErrorObj()
-        error.safe = """
-        UnoForm base cannot override base attributes while unoformsafe is True.
-        \tPlease:
-        \t\tChange the key '{}' to something else.
-        \tor
-        \t\tPass 'unoformsafe=False' during class instantiaion to be allowed to override object methods."""
-
-
-
-
-
-
+    raise Exception('NOPE.')
 
 
 class UnoFormatString(object):
