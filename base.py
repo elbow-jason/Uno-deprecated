b3f11bf5dd93ceaedd647d57818d2531e9942cf5
jlgoldb2 <jlgoldb2@asu.edu>
2014-07-24 18:18:31 -0700

    moved base to uno. duh.

--- base.py
+++ base.py
@@ -6,18 +6,7 @@ from flask import Markup
 try:
     from .errors import error
 except:
-    if __name__ == '__main__':
-        class ErrorObj(object):
-            pass
-
-        error = ErrorObj()
-        error.safe = """
-        UnoForm base cannot override base attributes while unoformsafe is True.
-        \tPlease:
-        \t\tChange the key '{}' to something else.
-        \tor
-        \t\tPass 'unoformsafe=False' during class instantiaion to be allowed to override object methods."""
-
+    raise Exception("YOU DID IT WRONG.")
 
 
 
